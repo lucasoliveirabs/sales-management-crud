e5b1f50941faeabb32a0a0343194f49529e50c8b
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2020-12-30 00:17:26 -0300

    add 'readById()' method in 'SaleDao'

--- sales-management-crud/src/main/java/com/xcompany/dao/SaleDao.java
+++ sales-management-crud/src/main/java/com/xcompany/dao/SaleDao.java
@@ -23,6 +23,7 @@ public class SaleDao implements Dao<Sale> {
 			+ "sale_delivery_adress=?, sale_observation=?, sale_status=?, sale_cancel_date=?, sale_cancel_reason=?"
 			+ "WHERE sale_id=?";
 	public static final String SQL_DELETE = "DELETE FROM sales WHERE sale_id=?";
+	public static final String SQL_READBYID = "SELECT * FROM sales WHERE sale_id=?";
 
 	public SaleDao(DataSource ds) {
 		datasource = ds;
@@ -66,23 +67,23 @@ public class SaleDao implements Dao<Sale> {
 			st = connection.prepareStatement(SQL_READALL);
 			rs = st.executeQuery();
 			while (rs.next()) {
-				Sale s = new Sale();
-				s.setSaleId(rs.getInt(1));
-				s.setProductId(rs.getInt(2));
-				s.setLeadId(rs.getInt(3));
-				s.setSaleDate(new java.util.Date(rs.getDate(4).getTime()));
-				s.setSaleDeliveryDate(new java.util.Date(rs.getDate(5).getTime()));
-				s.setSaleDeliveryAddress(rs.getString(6));
-				s.setSaleObs(rs.getString(7));
-				s.setSaleStatus(rs.getString(8));
+				Sale sale = new Sale();
+				sale.setSaleId(rs.getInt(1));
+				sale.setProductId(rs.getInt(2));
+				sale.setLeadId(rs.getInt(3));
+				sale.setSaleDate(new java.util.Date(rs.getDate(4).getTime()));
+				sale.setSaleDeliveryDate(new java.util.Date(rs.getDate(5).getTime()));
+				sale.setSaleDeliveryAddress(rs.getString(6));
+				sale.setSaleObs(rs.getString(7));
+				sale.setSaleStatus(rs.getString(8));
 				Date cancelDate = rs.getDate(9);
 				if (cancelDate == null) {
-					s.setSaleCancelDate(cancelDate);
+					sale.setSaleCancelDate(cancelDate);
 				} else {
-					s.setSaleCancelDate(new java.util.Date(cancelDate.getTime()));
+					sale.setSaleCancelDate(new java.util.Date(cancelDate.getTime()));
 				}
-				s.setSaleCancelReason(rs.getString(10));
-				list.add(s);
+				sale.setSaleCancelReason(rs.getString(10));
+				list.add(sale);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -129,6 +130,49 @@ public class SaleDao implements Dao<Sale> {
 		} finally {
 			close(connection, st);
 		}
+	}
+	
+	@Override
+	public Sale readById(String s) {
+		Connection connection = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		int saleId = 0;
+		Sale sale = null;
+		try {
+			connection = datasource.getConnection();
+			saleId = Integer.parseInt(s);
+			st = connection.prepareStatement(SQL_READBYID);
+			st.setInt(1, saleId);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				sale = new Sale();
+				sale.setSaleId(rs.getInt(1));
+				sale.setProductId(rs.getInt(2));
+				sale.setLeadId(rs.getInt(3));
+				sale.setSaleDate(new java.util.Date(rs.getDate(4).getTime()));
+				sale.setSaleDeliveryDate(new java.util.Date(rs.getDate(5).getTime()));
+				sale.setSaleDeliveryAddress(rs.getString(6));
+				sale.setSaleObs(rs.getString(7));
+				sale.setSaleStatus(rs.getString(8));
+				Date cancelDate = rs.getDate(9);
+				if (cancelDate == null) {
+					sale.setSaleCancelDate(cancelDate);
+				} else {
+					sale.setSaleCancelDate(new java.util.Date(cancelDate.getTime()));
+				}
+				sale.setSaleCancelReason(rs.getString(10));
+			} else {
+				throw new Exception("Id '"+saleId+"' not found.");
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(connection, st, rs);
+		}
+		return sale;
 	}
 
 	private void close(Connection connection, PreparedStatement st) {
