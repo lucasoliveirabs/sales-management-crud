6a7f6ef6a1550ea7a045ab9569cf1d37230b019a
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2021-01-08 18:46:05 -0300

    create 'UserDao' 'concrete Dao' class and fix 'authorizeLogin()' override method

---
+++ sales-management-crud/src/main/java/com/xcompany/dao/UserDao.java
@@ -0,0 +1,130 @@
+package com.xcompany.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.xcompany.model.User;
+
+public class UserDao implements Dao<User> {
+
+	private DataSource datasource;
+	public static final String SQL_CREATE = "";
+	public static final String SQL_UPDATE = "";
+	public static final String SQL_AUTHORIZELOGIN = "SELECT * FROM users WHERE user_username=? AND user_password=?";
+
+	public UserDao(DataSource ds) {
+		datasource = ds;
+	}
+
+	@Override
+	public User authorizeLogin(String username, String password) {
+		Connection connection = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		User user = null;
+		try {
+			connection = datasource.getConnection();
+			st = connection.prepareStatement(SQL_AUTHORIZELOGIN);
+			st.setString(1, username);
+			st.setString(2, password);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				user = new User();
+				user.setUserId(rs.getInt(1));
+				user.setUserEmail(rs.getString(2));
+				user.setUserPassword(rs.getString(3));
+				user.setUserFirstName(rs.getString(4));
+				user.setUserLastName(rs.getString(5));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(connection, st, rs);
+		}
+		return user;
+	}
+
+	@Override
+	public void create(User t) {
+		Connection connection = null;
+		PreparedStatement st = null;
+		try {
+			connection = datasource.getConnection();
+			st = connection.prepareStatement(SQL_CREATE);
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(connection, st, null);
+		}
+	}
+
+	@Override
+	public void update(User t) {
+		Connection connection = null;
+		PreparedStatement st = null;
+		try {
+			connection = datasource.getConnection();
+			st = connection.prepareStatement(SQL_UPDATE);
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(connection, st, null);
+		}
+	}
+
+	@Override
+	public void delete(Integer i) {
+	}
+
+	@Override
+	public User readById(String s) {
+		return null;
+	}
+
+	@Override
+	public List<User> readAll() {
+		return null;
+	}
+
+	private void close(Connection connection, PreparedStatement st, ResultSet rs) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Connection closing failure");
+			}
+			connection = null;
+		}
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Statement closing failure");
+			}
+			st = null;
+		}
+
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("ResultSet closing failure");
+			}
+			rs = null;
+		}
+	}
+}
