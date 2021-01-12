4cd178ad86bc382d0922368c0971f69361b9ba91
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2021-01-11 23:27:45 -0300

    add 'createUser()', 'updateUser()' and 'logout()' controller methods

--- sales-management-crud/src/main/java/com/xcompany/controller/UserServlet.java
+++ sales-management-crud/src/main/java/com/xcompany/controller/UserServlet.java
@@ -8,9 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-import com.xcompany.dao.SaleDao;
 import com.xcompany.dao.UserDao;
 import com.xcompany.model.User;
 
@@ -34,7 +34,6 @@ public class UserServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -42,31 +41,83 @@ public class UserServlet extends HttpServlet {
 		try {
 			String command = request.getParameter("command");
 			if (command == null) {
-				response.sendRedirect("login.jsp");
+				command = "LOGOUT";
 			}
 			
 			switch (command) {
 			case "LOGIN":
 				verifyLogin(request, response);
+				break;
+			case "ADD":
+				createUser(request, response);
+				break;
+			case "LOGOUT":
+				logout(request, response);
+				break;
+			case "UPDATE":
+				update(request, response);
+				break;
+			default:
+				logout(request, response);
 			}
-			
+
 		} catch (Exception e) {
 			throw new ServletException(e);
 		}
 	}
-
+	
+	private void createUser(HttpServletRequest request, HttpServletResponse response)throws Exception {
+		String username = request.getParameter("newUsername");
+		String password = request.getParameter("confirmedNewPassword");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		
+		User user = new User();
+		user.setUserUsername(username);
+		user.setUserPassword(password);
+		user.setUserFirstName(firstName);
+		user.setUserLastName(lastName);
+		
+		dao.create(user);
+		response.sendRedirect("login.jsp");
+	}	
+	
+	private void logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		session.removeAttribute("user");
+		session.invalidate();
+		response.sendRedirect("login.jsp");
+	}
+	
+	private void update(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int userId = Integer.parseInt(request.getParameter("userId"));
+		String username = request.getParameter("newUsername");
+		String password = request.getParameter("newPassword");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		
+		User user = new User();
+		user.setUserId(userId);
+		user.setUserUsername(username);
+		user.setUserPassword(password);
+		user.setUserFirstName(firstName);
+		user.setUserLastName(lastName);
+		
+		dao.update(user);
+		response.sendRedirect("SaleServlet");
+	}
+	
 	private void verifyLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-
 		User user = dao.authorizeLogin(username, password);
 
 		if (user == null) {
 			response.sendRedirect("login.jsp");
 		} else {
-			
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
 			response.sendRedirect("SaleServlet");
 		}
 	}
-
 }
