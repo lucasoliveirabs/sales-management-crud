d8b5d8b973e95a544f462a8c7f1195dcc34bb216
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2021-01-11 23:32:01 -0300

    add 'create()' and 'update()' 'UserDao' methods

--- sales-management-crud/src/main/java/com/xcompany/dao/UserDao.java
+++ sales-management-crud/src/main/java/com/xcompany/dao/UserDao.java
@@ -13,8 +13,8 @@ import com.xcompany.model.User;
 public class UserDao implements Dao<User> {
 
 	private DataSource datasource;
-	public static final String SQL_CREATE = "";
-	public static final String SQL_UPDATE = "";
+	public static final String SQL_CREATE = "INSERT INTO users (user_username, user_password, user_first_name, user_last_name) VALUES (?, ?, ?, ?)";
+	public static final String SQL_UPDATE = "UPDATE users SET user_username=?, user_password=?, user_first_name=?, user_last_name=? WHERE user_id=?";
 	public static final String SQL_AUTHORIZELOGIN = "SELECT * FROM users WHERE user_username=? AND user_password=?";
 
 	public UserDao(DataSource ds) {
@@ -36,7 +36,7 @@ public class UserDao implements Dao<User> {
 			if (rs.next()) {
 				user = new User();
 				user.setUserId(rs.getInt(1));
-				user.setUserEmail(rs.getString(2));
+				user.setUserUsername(rs.getString(2));
 				user.setUserPassword(rs.getString(3));
 				user.setUserFirstName(rs.getString(4));
 				user.setUserLastName(rs.getString(5));
@@ -52,13 +52,16 @@ public class UserDao implements Dao<User> {
 	}
 
 	@Override
-	public void create(User t) {
+	public void create(User user) {
 		Connection connection = null;
 		PreparedStatement st = null;
 		try {
 			connection = datasource.getConnection();
 			st = connection.prepareStatement(SQL_CREATE);
-
+			st.setString(1, user.getUserUsername());
+			st.setString(2, user.getUserPassword());
+			st.setString(3, user.getUserFirstName());
+			st.setString(4, user.getUserLastName());
 			st.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -68,13 +71,17 @@ public class UserDao implements Dao<User> {
 	}
 
 	@Override
-	public void update(User t) {
+	public void update(User user) {
 		Connection connection = null;
 		PreparedStatement st = null;
 		try {
 			connection = datasource.getConnection();
 			st = connection.prepareStatement(SQL_UPDATE);
-
+			st.setString(1, user.getUserUsername());
+			st.setString(2, user.getUserPassword());
+			st.setString(3, user.getUserFirstName());
+			st.setString(4, user.getUserLastName());
+			st.setInt(5, user.getUserId());
 			st.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
